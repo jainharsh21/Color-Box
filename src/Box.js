1ad898a370d4994873e6127161aaf7368ca35049
HARSH JAIN <jainharsh270@gmail.com>
2020-04-09 18:42:07 +0530

    passing the color array from parent to child

--- src/Box.js
+++ src/Box.js
@@ -3,25 +3,15 @@ import { choice } from "./helpers";
 import "./Box.css";
 
 class Box extends Component {
-  static defaultProps = {
-    allColors: [
-      "purple",
-      "magenta",
-      "darkslateblue",
-      "pink",
-      "orange",
-      "orangered",
-    ],
-  };
   constructor(props) {
     super(props);
-    this.state = { color: choice(this.props.allColors) };
+    this.state = { color: choice(this.props.colors) };
     this.handleClick = this.handleClick.bind(this);
   }
   changeColor() {
     let newColor;
     do {
-      newColor = choice(this.props.allColors);
+      newColor = choice(this.props.colors);
     } while (newColor === this.state.color);
     this.setState({ color: newColor });
   }
--- src/BoxContainer.js
+++ src/BoxContainer.js
@@ -1,14 +1,23 @@
 import React, { Component } from "react";
 import Box from "./Box";
+import "./BoxContainer.css";
 
 class BoxContainer extends Component {
   static defaultProps = {
     numBoxes: 18,
+    allColors: [
+      "purple",
+      "magenta",
+      "darkslateblue",
+      "pink",
+      "orange",
+      "orangered",
+    ],
   };
 
   render() {
     const boxes = Array.from({ length: this.props.numBoxes }).map(() => (
-      <Box />
+      <Box colors={this.props.allColors} />
     ));
     return <div className="BoxContainer">{boxes}</div>;
   }
