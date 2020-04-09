4addd75e2600fd294afcb72fbc8e66ff359f180c
HARSH JAIN <jainharsh270@gmail.com>
2020-04-09 18:45:11 +0530

    Updates

---
+++ precache-manifest.4ac4180ebceb79fcbc5561e4058c20be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ce3c1b0d48abf836d870c9be12c7bdf",
+    "url": "/Color-Box/index.html"
+  },
+  {
+    "revision": "80e1e6eb7048cf716588",
+    "url": "/Color-Box/static/css/main.42aef36e.chunk.css"
+  },
+  {
+    "revision": "85fb7dc0738666df114a",
+    "url": "/Color-Box/static/js/2.80b8b1de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Color-Box/static/js/2.80b8b1de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "80e1e6eb7048cf716588",
+    "url": "/Color-Box/static/js/main.22fdcd00.chunk.js"
+  },
+  {
+    "revision": "5107fbc6ecca2e0c95ae",
+    "url": "/Color-Box/static/js/runtime-main.9753e963.js"
+  }
+]);
